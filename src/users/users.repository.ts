a404206fe79418ff78a778b4c3f7a3717c2bb675
BorovskovaYevheniia <borovskova.yevheniia@lazy-ants.net>
2023-05-27 23:25:30 +0300

    added app config

---
+++ src/users/users.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { FilterQuery, Model } from "mongoose";
+
+import { User, UserDocument } from "./schemas/user.schema";
+
+@Injectable()
+export class UsersRepository {
+
+    constructor(@InjectModel(User.name) private _userModel:Model<UserDocument>){
+
+    }
+
+    public async findOne(userFilterQuery:FilterQuery<User>):Promise<User>{
+        return this._userModel.findOne(userFilterQuery)
+    }
+
+    public async find(userFilterQuery:FilterQuery<User>):Promise<User[]>{
+        return this._userModel.find(userFilterQuery)
+    }
+
+    public async create(user:User):Promise<User[]>{
+        const newUser:any = new this._userModel(user);
+        return newUser.save()
+    }
+
+    public async findOneAndUpdate(userFilterQuery:FilterQuery<User>, user:Partial<User>):Promise<User>{
+        return this._userModel.findOneAndUpdate(userFilterQuery, user)
+    }
+}
